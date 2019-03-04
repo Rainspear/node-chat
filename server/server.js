b37ca0aea2a713d000a670395ec3879af43efba6
Khang <invisibledark3@gmail.com>
2019-03-04 19:39:15 +0700

    update port

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 9999;
 console.log(pubpath);
 
 app.use(express.static(pubpath));
-app.listen(9999, () => {
+app.listen(port, () => {
     console.log(`server node-chat is running on port ${port}`);
 });
