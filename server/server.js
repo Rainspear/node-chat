e722de981771ef8b0bfabc86bce6535bef0fabd2
Khang <invisibledark3@gmail.com>
2019-03-05 21:20:56 +0700

    fix callback is not function issue

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on("connection", (socket) => {
         // callback here mean the function from index.js
         console.log("createMessenger", messenger);
         io.emit("newMessenger", generateMessage(messenger.from, messenger.text));
-        callback("ack success");
+        callback("got it");
     });
 
 })
